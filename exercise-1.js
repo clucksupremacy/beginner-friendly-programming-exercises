2ecdc73727a53faaad324d6ac54956a816ce9e81
clucksupremacy <cherrycluckcluck@gmail.com>
2024-07-13 14:19:14 -0700

    completed exercise

--- exercise-1.js
+++ exercise-1.js
@@ -1,1 +1,14 @@
-test
+function swap(a, b) {
+    first_value = a; 
+    second_value = b; 
+
+    a = second_value; 
+    b = first_value; 
+    
+    return [a, b];
+}
+
+// to access a and b
+let [a, b] = swap(11, 22);
+console.log(a);
+console.log(b);
