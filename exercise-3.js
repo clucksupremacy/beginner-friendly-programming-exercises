2caa716083a81ebf42735108121125a6a8b2bdea
clucksupremacy <cherrycluckcluck@gmail.com>
2024-07-15 19:21:40 -0700

    first version slight mod

--- exercise-3.js
+++ exercise-3.js
@@ -1,10 +1,16 @@
 function bitcoin_calculator(current_value, percent_change) {
     let decimal_change = percent_change * 0.01;
-    let percent_value = current_value * decimal_change;
+    let percent_value = Math.abs(current_value) * decimal_change;
     let total_value = current_value + percent_value; 
 
     console.log("Total bitcoin value: ", total_value);
-    console.log("Amount increased or decreased: ", percent_value);
+    console.log("Value increase/decrease: ", percent_value);
+
+    return [total_value, percent_value];
 }
 
-bitcoin_calculator(10000, 10);
+// tests
+// bitcoin_calculator(10000, 10);  // Expected: 11000, 1000
+// bitcoin_calculator(-10000, 10); // Expected: -9000, 1000
+// bitcoin_calculator(10000, -10); // Expected: 9000, -1000
+// bitcoin_calculator(-10000, -10);// Expected: -11000, -1000
