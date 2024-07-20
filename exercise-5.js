3ce65de788f6d81c5345fce98c14e23ff13bf610
clucksupremacy <cherrycluckcluck@gmail.com>
2024-07-19 19:08:24 -0700

    need to fix some logic

--- exercise-5.js
+++ exercise-5.js
@@ -1,17 +1,19 @@
 function crypto_calculator(money, bitcoin_price, ethereum_price, litecoin_price) {
-    function has_money(money) {
-        if (money <= 0) {
-            console.log("Insufficient funds");
-            return false;
-        }
-        return true; 
+    if (money <= 0) {
+        console.log("Insufficient funds");
+        return undefined;
     }
 
-    function coin_has_price(price) {
-        if (price <= 0) {
-            return false; 
-        }
-        return true; 
+    if (bitcoin_price <= 0) {
+       bitcoin_price = undefined;
+    }
+
+    if (ethereum_price <= 0) {
+        ethereum_price = undefined;
+    }
+
+    if (litecoin_price <= 0) {
+        litecoin_price = undefined;
     }
 
     function can_buy(money, coin_price) {
@@ -22,10 +24,9 @@ function crypto_calculator(money, bitcoin_price, ethereum_price, litecoin_price)
         return true; 
     }
 
-    if (!has_money(money) || 
-        // !coin_has_price(bitcoin_price) || 
-        // !coin_has_price(ethereum_price) || 
-        // !coin_has_price(litecoin_price) || 
+    if (/* !coin_has_price(bitcoin_price) || 
+        !coin_has_price(ethereum_price) || 
+        !coin_has_price(litecoin_price) || */ 
         !can_buy(money, bitcoin_price) || 
         !can_buy(money, ethereum_price) || 
         !can_buy(money, litecoin_price)) 
@@ -37,6 +38,8 @@ function crypto_calculator(money, bitcoin_price, ethereum_price, litecoin_price)
     const ethereum_max = Math.floor(money / ethereum_price);
     const litecoin_max = Math.floor(money / litecoin_price);
 
+    console.log("With ", money, "$ you can buy: ", bitcoin_max, " Bitcoins, ", ethereum_max, " Ethereum, and ", litecoin_max, " Litecoins");
+
     return [bitcoin_max, ethereum_max, litecoin_max];
 }
 // tests
@@ -44,3 +47,4 @@ function crypto_calculator(money, bitcoin_price, ethereum_price, litecoin_price)
 // console.log(crypto_calculator(-100, 50, 20, 10));
 // console.log(crypto_calculator(100, 50.25, 20.001, 10));
 console.log(crypto_calculator(100, -50, 20, 10));
+// console.log(crypto_calculator(100, -50, 200, 10));
