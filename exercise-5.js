f07c77d26e176dbf11ed9f8acdce3e2703bf3697
clucksupremacy <cherrycluckcluck@gmail.com>
2024-07-19 21:14:15 -0700

    remove some comments

--- exercise-5.js
+++ exercise-5.js
@@ -50,11 +50,3 @@ function crypto_calculator(money, bitcoin_price, ethereum_price, litecoin_price)
 // console.log(crypto_calculator(100, -50, 200, 10));
 // console.log(crypto_calculator(0, -50, 200, 10));
 // console.log(crypto_calculator(1, -50, 200, 10));
-
-//special conditions:
-    //if money is less than or equal to 0, can't buy anything. Return "Insufficient funds" and [0,0,0]
-    //if have money but one or more stock is less than 0 (negative), stock is undefined. Return "Undefined/NaN" for that stock, but return the full array.
-    //if have money but one or more stock is equal to 0, you can buy "0" of that stock. Return full array.
-    //if have money but one or more stock's price exceeds money, can't buy that stock. "0" for that stock, print full array. 
-
-    //clean up later
