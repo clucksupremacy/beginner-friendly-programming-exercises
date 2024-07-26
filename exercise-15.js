7f9165c0628ab61aa14ddad58b8c992361a5232d
clucksupremacy <cherrycluckcluck@gmail.com>
2024-07-26 15:43:17 -0700

    phone_bill function

---
+++ exercise-15.js
@@ -0,0 +1,18 @@
+function phone_bill(seconds) {
+    let amount;
+
+    if (seconds < 1) {
+        amount = 0;
+    } else if ((seconds >= 1) && (seconds <= 500)) {
+        amount = seconds * 0.01;
+    } else if ((seconds >= 501) && (seconds <= 800)) {
+        amount = 5 + ((seconds - 500) * 0.008); // amount = (500 * 0.01) + ((seconds - 500) * 0.008); 
+    } else if (seconds >= 801) {
+        amount = 7.4 + ((seconds - 800) * 0.005); // amount = (500 * 0.01) + (300 * 0.008) + ((seconds - 800) * 0.005);
+    }
+
+    let total_amount = amount + 25;
+    return total_amount;
+}
+
+// console.log(phone_bill(801));
