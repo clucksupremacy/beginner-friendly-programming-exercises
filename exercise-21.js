ceeffb5a84e5bb9e1212b8a974690ec02ffe31a1
clucksupremacy <cherrycluckcluck@gmail.com>
2024-08-01 19:09:14 -0700

    function for average of values in array

--- exercise-21.js
+++ exercise-21.js
@@ -1,5 +1,13 @@
-function average_5(a, b, c, d, e) {
-    return (a + b + c + d + e) / 5;
+function average_5(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    let average = sum / arr.length;
+    
+    return average;
 }
 
-console.log("The average is", average_5(4, 6, 1, 4, 9));
+console.log("The average is", average_5([4, 6, 1, 4, 9]));
