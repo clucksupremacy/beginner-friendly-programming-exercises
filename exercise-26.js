e04cdb48d3308989cd69f173cd35ce83153912cf
clucksupremacy <cherrycluckcluck@gmail.com>
2024-08-23 18:24:58 -0700

    add functions to compute and compare prices

--- exercise-26.js
+++ exercise-26.js
@@ -1,11 +1,12 @@
-function compare_rent() {
+function rent_prices() {
     let average_rent = 0;
     let apts_registered = [];
-    let apt_of_interest = 0;
+    let number_input = 0;
+    let sum = 0;
 
     function collect_apt_prices() {
         const prompt = require("prompt-sync")({ sigint: true });
-        input = prompt('Enter apartment price. Press 0 or enter a negative value to exit: ');
+        input = prompt('Enter apartment price to add to register. Press 0 or enter a negative value to exit: ');
 
         if (input <= 0) {
             console.log("Exiting");
@@ -13,12 +14,43 @@ function compare_rent() {
         } else if (isNaN(input)) {
             collect_apt_prices();
         } else {
-            apts_registered.push(input);
+            number_input = Number(input);
+            apts_registered.push(number_input);
             collect_apt_prices();
         }
-    }
-    collect_apt_prices();
-    console.log(apts_registered);
-}
+    } collect_apt_prices();
 
-compare_rent();
+    function calculate_avg_price() {
+        for (i = 0; i < apts_registered.length; i++) {
+            sum += apts_registered[i];
+        }
+    
+        average_rent = sum / (apts_registered.length);
+        // average_rent = apts_registered.reduce((a, b) => a + b, 0); 
+            // the start of an alternative method
+    
+        console.log(apts_registered.length, "apartments have registed. The average price for rent is ",     average_rent, "$"); // keeping the funky spelling errors
+
+        return average_rent;
+    } calculate_avg_price();
+
+    function compare_prices() {
+        const prompt = require("prompt-sync")({ sigint: true });
+        input = prompt('Enter apartment price to compare with average rent price. Press 0 or enter a negative value to exit: ');
+
+        if (input <= 0) {
+            console.log("Quit Program");
+        } else if (input == average_rent) {
+            console.log("Equal to average price"); // added an equals condition for consistency
+            compare_prices();
+        } else if (input < average_rent) {
+            console.log("Below average price");
+            compare_prices();
+        } else if (input > average_rent) {
+            console.log("Above average price");
+            compare_prices();
+        } else {
+            compare_prices();
+        }
+    } compare_prices();
+} rent_prices();
