8f0c93fde206f97686ae37221ee745ca829de4f3
clucksupremacy <cherrycluckcluck@gmail.com>
2024-09-11 17:41:58 -0700

    function applies generally

--- exercise-28.js
+++ exercise-28.js
@@ -1,7 +1,7 @@
 // instructions unclear. Doing it my way.
 const prompt = require("prompt-sync")({ sigint: true });
 
-function countdown() {
+function start() {
     let input = 0;
     let zero_or_greater = 0;
 
@@ -9,13 +9,25 @@ function countdown() {
         input = prompt('Enter a non-negative integer to run. Enter a negative integer to exit: ');
         
         if (input < 0) {
-            console.log('Exit Program');
-            console.log(zero_or_greater);
+            console.log('Countdown begins');
             return zero_or_greater;
         } else if (input > -1) {
             zero_or_greater = input;
         }
     }
     while (input);
+}
+
+function countdown() {
+    let num_start = start();
+
+    let intervalId = setInterval(() => {
+        if (num_start > 0) {
+            console.log(num_start--);
+        } else if (num_start == 0) {
+            console.log("Go!");
+            clearInterval(intervalId);
+        }
+      }, 1000);
 }
 countdown();
